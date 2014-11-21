f7f7674de80dbdeaa471a08b6cea318fa583fad9
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-11-21 12:00:46 +0100

    first implementation

---
+++ jsonschema.go
@@ -0,0 +1,142 @@
+package defaults
+
+import (
+	"encoding/json"
+	"reflect"
+	"strings"
+)
+
+type JSONSchema struct {
+	Type       string                 `json:"type"`
+	Items      *JSONSchemaItems       `json:"items,omitempty"`
+	Properties map[string]*JSONSchema `json:"properties,omitempty"`
+	Required   []string               `json:"required,omitempty"`
+}
+
+type JSONSchemaItems struct {
+	Type string `json:"type,omitempty"`
+}
+
+func (j *JSONSchema) Marshal() ([]byte, error) {
+	return json.MarshalIndent(j, "", "  ")
+}
+
+func (j *JSONSchema) String() string {
+	json, _ := j.Marshal()
+	return string(json)
+}
+
+func (j *JSONSchema) Load(variable interface{}) {
+	value := reflect.ValueOf(variable).Elem()
+	j.doLoad(value, tagOptions(""))
+}
+
+func (j *JSONSchema) doLoad(value reflect.Value, opts tagOptions) {
+	kind := value.Kind()
+
+	j.Type = getTypeFromMapping(kind)
+	switch kind {
+	case reflect.Slice:
+		j.doLoadFromSlice(value)
+	case reflect.Map:
+		j.doLoadFromMap(value)
+	case reflect.Struct:
+		j.doLoadFromStruct(value)
+	}
+}
+
+func (j *JSONSchema) doLoadFromSlice(valueObject reflect.Value) {
+	k := valueObject.Type().Elem().Kind()
+	if k == reflect.Uint8 {
+		j.Type = "string"
+	} else {
+		j.Items = &JSONSchemaItems{Type: getTypeFromMapping(k)}
+	}
+}
+
+func (j *JSONSchema) doLoadFromMap(valueObject reflect.Value) {
+	k := valueObject.Type().Elem().Kind()
+	j.Properties = make(map[string]*JSONSchema, 0)
+	j.Properties[".*"] = &JSONSchema{Type: getTypeFromMapping(k)}
+}
+
+func (j *JSONSchema) doLoadFromStruct(valueObject reflect.Value) {
+	j.Type = "object"
+	j.Properties = make(map[string]*JSONSchema, 0)
+	typeObject := valueObject.Type()
+
+	count := valueObject.NumField()
+	for i := 0; i < count; i++ {
+		field := typeObject.Field(i)
+		value := valueObject.Field(i)
+
+		tag := field.Tag.Get("json")
+		name, opts := parseTag(tag)
+		if name == "" {
+			name = field.Name
+		}
+
+		j.Properties[name] = &JSONSchema{}
+		j.Properties[name].doLoad(value, opts)
+
+		if !opts.Contains("omitempty") {
+			j.Required = append(j.Required, name)
+		}
+	}
+}
+
+var mapping = map[reflect.Kind]string{
+	reflect.Bool:    "bool",
+	reflect.Int:     "integer",
+	reflect.Int8:    "integer",
+	reflect.Int16:   "integer",
+	reflect.Int32:   "integer",
+	reflect.Int64:   "integer",
+	reflect.Uint:    "integer",
+	reflect.Uint8:   "integer",
+	reflect.Uint16:  "integer",
+	reflect.Uint32:  "integer",
+	reflect.Uint64:  "integer",
+	reflect.Float32: "number",
+	reflect.Float64: "number",
+	reflect.String:  "string",
+	reflect.Slice:   "array",
+	reflect.Struct:  "object",
+	reflect.Map:     "object",
+}
+
+func getTypeFromMapping(k reflect.Kind) string {
+	if t, ok := mapping[k]; ok {
+		return t
+	}
+
+	return ""
+}
+
+type tagOptions string
+
+func parseTag(tag string) (string, tagOptions) {
+	if idx := strings.Index(tag, ","); idx != -1 {
+		return tag[:idx], tagOptions(tag[idx+1:])
+	}
+	return tag, tagOptions("")
+}
+
+func (o tagOptions) Contains(optionName string) bool {
+	if len(o) == 0 {
+		return false
+	}
+	s := string(o)
+	for s != "" {
+		var next string
+		i := strings.Index(s, ",")
+		if i >= 0 {
+			s, next = s[:i], s[i+1:]
+		}
+		if s == optionName {
+			return true
+		}
+		s = next
+	}
+	return false
+}
---
+++ jsonschema_test.go
@@ -0,0 +1,116 @@
+package defaults
+
+import (
+	"testing"
+
+	. "launchpad.net/gocheck"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+type JSONSchemaSuite struct{}
+
+var _ = Suite(&JSONSchemaSuite{})
+
+type ExampleJSONBasic struct {
+	Bool       bool    `json:",omitempty"`
+	Integer    int     `json:",omitempty"`
+	Integer8   int8    `json:",omitempty"`
+	Integer16  int16   `json:",omitempty"`
+	Integer32  int32   `json:",omitempty"`
+	Integer64  int64   `json:",omitempty"`
+	UInteger   uint    `json:",omitempty"`
+	UInteger8  uint8   `json:",omitempty"`
+	UInteger16 uint16  `json:",omitempty"`
+	UInteger32 uint32  `json:",omitempty"`
+	UInteger64 uint64  `json:",omitempty"`
+	String     string  `json:",omitempty"`
+	Bytes      []byte  `json:",omitempty"`
+	Float32    float32 `json:",omitempty"`
+	Float64    float64
+}
+
+func (self *JSONSchemaSuite) TestLoad(c *C) {
+	j := &JSONSchema{}
+	j.Load(&ExampleJSONBasic{})
+
+	c.Assert(*j, DeepEquals, JSONSchema{
+		Type:     "object",
+		Required: []string{"Float64"},
+		Properties: map[string]*JSONSchema{
+			"Bool":       &JSONSchema{Type: "bool"},
+			"Integer":    &JSONSchema{Type: "integer"},
+			"Integer8":   &JSONSchema{Type: "integer"},
+			"Integer16":  &JSONSchema{Type: "integer"},
+			"Integer32":  &JSONSchema{Type: "integer"},
+			"Integer64":  &JSONSchema{Type: "integer"},
+			"UInteger":   &JSONSchema{Type: "integer"},
+			"UInteger8":  &JSONSchema{Type: "integer"},
+			"UInteger16": &JSONSchema{Type: "integer"},
+			"UInteger32": &JSONSchema{Type: "integer"},
+			"UInteger64": &JSONSchema{Type: "integer"},
+			"String":     &JSONSchema{Type: "string"},
+			"Bytes":      &JSONSchema{Type: "string"},
+			"Float32":    &JSONSchema{Type: "number"},
+			"Float64":    &JSONSchema{Type: "number"},
+		},
+	})
+}
+
+type ExampleJSONBasicWithTag struct {
+	Bool bool `json:"test"`
+}
+
+func (self *JSONSchemaSuite) TestLoadWithTag(c *C) {
+	j := &JSONSchema{}
+	j.Load(&ExampleJSONBasicWithTag{})
+
+	c.Assert(*j, DeepEquals, JSONSchema{
+		Type:     "object",
+		Required: []string{"test"},
+		Properties: map[string]*JSONSchema{
+			"test": &JSONSchema{Type: "bool"},
+		},
+	})
+
+}
+
+type ExampleJSONBasicSlices struct {
+	Strings []string `json:",omitempty"`
+}
+
+func (self *JSONSchemaSuite) TestLoadSlice(c *C) {
+	j := &JSONSchema{}
+	j.Load(&ExampleJSONBasicSlices{})
+
+	c.Assert(*j, DeepEquals, JSONSchema{
+		Type: "object",
+		Properties: map[string]*JSONSchema{
+			"Strings": &JSONSchema{
+				Type:  "array",
+				Items: &JSONSchemaItems{Type: "string"},
+			},
+		},
+	})
+}
+
+type ExampleJSONBasicMaps struct {
+	Maps map[string]string `json:",omitempty"`
+}
+
+func (self *JSONSchemaSuite) TestLoadMap(c *C) {
+	j := &JSONSchema{}
+	j.Load(&ExampleJSONBasicMaps{})
+
+	c.Assert(*j, DeepEquals, JSONSchema{
+		Type: "object",
+		Properties: map[string]*JSONSchema{
+			"Maps": &JSONSchema{
+				Type: "object",
+				Properties: map[string]*JSONSchema{
+					".*": &JSONSchema{Type: "string"},
+				},
+			},
+		},
+	})
+}

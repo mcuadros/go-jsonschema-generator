0dd7d6581d2a395c90f3a3f28cf7e7a11d5d97ce
cj2 <cj@marucreative.com>
2016-11-18 16:12:53 -0700

    Update bool type -> boolean

--- jsonschema_test.go
+++ jsonschema_test.go
@@ -13,7 +13,7 @@ type propertySuite struct{}
 var _ = Suite(&propertySuite{})
 
 type ExampleJSONBasic struct {
-        Omitted    string  `json:"-,omitempty"`
+	Omitted    string  `json:"-,omitempty"`
 	Bool       bool    `json:",omitempty"`
 	Integer    int     `json:",omitempty"`
 	Integer8   int8    `json:",omitempty"`
@@ -42,7 +42,7 @@ func (self *propertySuite) TestLoad(c *C) {
 			Type:     "object",
 			Required: []string{"Float64", "Interface"},
 			Properties: map[string]*property{
-				"Bool":       &property{Type: "bool"},
+				"Bool":       &property{Type: "boolean"},
 				"Integer":    &property{Type: "integer"},
 				"Integer8":   &property{Type: "integer"},
 				"Integer16":  &property{Type: "integer"},
@@ -77,7 +77,7 @@ func (self *propertySuite) TestLoadWithTag(c *C) {
 			Type:     "object",
 			Required: []string{"test"},
 			Properties: map[string]*property{
-				"test": &property{Type: "bool"},
+				"test": &property{Type: "boolean"},
 			},
 		},
 	})
@@ -189,7 +189,7 @@ func (self *propertySuite) TestString(c *C) {
 
 	expected := "{\n" +
 		"    \"$schema\": \"http://json-schema.org/schema#\",\n" +
-		"    \"type\": \"bool\"\n" +
+		"    \"type\": \"boolean\"\n" +
 		"}"
 
 	c.Assert(j.String(), Equals, expected)
